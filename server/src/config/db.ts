c090d6a11e6417fc43689920469805a050eec774
Chaeah Park <102865440+chepark@users.noreply.github.com>
2023-01-01 16:49:17 +0200

    Setup MySQL in AWS.

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,11 +1,11 @@
 import mysql from "mysql";
 
 const db = mysql.createConnection({
-  host: "database-1.cxdogurmr268.us-east-1.rds.amazonaws.com",
-  post: "3306",
-  user: "admin",
-  password: "BEs0731l!",
-  database: "no_time_zone",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 db.connect((error) => {
