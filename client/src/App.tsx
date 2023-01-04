9b980ed8c2c9c011e4b39e6c61f9c529a5dd389d
Chaeah Park <102865440+chepark@users.noreply.github.com>
2023-01-04 13:16:43 +0200

    Setup EventSource to receive Server Sent Event.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
+import CoordinatePlane from "./components/CoordinatePlane";
+import ViolatorTable from "./components/ViolatorTable";
+import { SERVER_SENT_EVENT_URL } from "./constants/constants";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const events = new EventSource(SERVER_SENT_EVENT_URL);
+    events.onmessage = (e) => {
+      const parsedData = JSON.parse(e.data);
+      console.log(parsedData);
+    };
+  }, []);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="App border-solid border-2 border-indigo-800 container">
+      <h1>Project BirdNest</h1>
+      <h2>Who bothers the endangered bird specy, Great crested grebe?</h2>
+      <main className="grid grid-cols-2">
+        <CoordinatePlane />
+        <ViolatorTable />
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
